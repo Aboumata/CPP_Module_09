6e07f9b7a80fc4ff91feb3489793c7e508fce56b
Aboumata <fanchkhow@gmail.com>
2026-08-31 21:58:23 +0100

    Refactor Makefile and implement RPN class skeleton with initial data files.

--- ex00/RPN.cpp
+++ ex00/RPN.cpp
@@ -1,2 +1,18 @@
 #include "RPN.hpp"
 
+RPN::RPN() {
+
+}
+
+RPN::RPN(const RPN &other) {
+    (void) other;
+}
+
+RPN &RPN::operator=(const RPN &other) {
+    (void) other;
+    return *this;
+}
+
+RPN::~RPN() {
+
+}
--- ex00/RPN.hpp
+++ ex00/RPN.hpp
@@ -4,6 +4,9 @@
 class RPN {
 public:
     RPN();
+    RPN(const RPN& other);
+    RPN& operator=(const RPN& other);
+    ~RPN();
 
 };
 
